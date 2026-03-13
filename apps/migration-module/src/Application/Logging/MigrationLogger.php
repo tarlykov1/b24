56b8753325ca27341caff47dc609a30a89d8f943
tarlykov1 <44412162+tarlykov1@users.noreply.github.com>
2026-03-13 22:37:32 +0300

    Add RU/EN internationalization layer for migration admin

--- apps/migration-module/src/Application/Logging/MigrationLogger.php
+++ apps/migration-module/src/Application/Logging/MigrationLogger.php
@@ -4,12 +4,25 @@ declare(strict_types=1);
 
 namespace MigrationModule\Application\Logging;
 
+use MigrationModule\Application\I18n\BackendMessageTranslator;
 use MigrationModule\Domain\Log\LogRecord;
 
 final class MigrationLogger
 {
+    private BackendMessageTranslator $translator;
+
+    public function __construct(?BackendMessageTranslator $translator = null)
+    {
+        $this->translator = $translator ?? new BackendMessageTranslator();
+    }
+
     public function log(LogRecord $record): void
     {
         // TODO: write structured logs to migration_log and system sink.
+    }
+
+    public function localizeMessageCode(string $messageCode, string $locale = 'en'): string
+    {
+        return $this->translator->translate($messageCode, $locale);
     }
 }
